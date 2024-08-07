ecbec6425a883b7e28658d757afa70358e5e0f07
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-08-07 22:55:23 +0100

    eslint for utils

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // DBClient class
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 const DBClient = class {
   constructor() {
@@ -8,17 +8,25 @@ const DBClient = class {
     const PORT = process.env.DB_PORT || 27017;
     const DB = process.env.DB_DATABASE || 'files_manager';
     const uri = `mongodb://${HOST}:${PORT}`;
-    this.client = new MongoClient(uri, {useUnifiedTopology: true});
+    this.client = new MongoClient(uri, { useUnifiedTopology: true });
     (async () => {
       await this.client.connect();
       this.db = await this.client.db(DB);
     })();
   }
 
-  isAlive = () => Boolean(this.db); // this.client.isConnected(); [Deprecated]
-  nbUsers = async () => await this.db.collection('users').countDocuments();
-  nbFiles = async () => await this.db.collection('files').countDocuments();
-}
+  isAlive() { return Boolean(this.db); } // this.client.isConnected(); [Deprecated]
+
+  async nbUsers() {
+    const nbUsers = await this.db.collection('users').countDocuments();
+    return nbUsers;
+  }
+
+  async nbFiles() {
+    const nbFiles = await this.db.collection('files').countDocuments();
+    return nbFiles;
+  }
+};
 
 const dbClient = new DBClient();
 export default dbClient;
--- utils/redis.js
+++ utils/redis.js
@@ -3,22 +3,28 @@
 import redis from 'redis';
 import util from 'util';
 
-class RedisClient {
+const RedisClient = class {
   constructor() {
     this.isConnected = true;
     this.client = redis.createClient()
       .on('error', (err) => {
-	console.log(err);
-	this.isConnected = false;
+        console.log(err);
+        this.isConnected = false;
       });
     this.getAsync = util.promisify(this.client.get).bind(this.client);
   }
 
-  isAlive = () => this.isConnected;
-  get = async (key) => this.getAsync(key);
-  set = async (key, value, duration) => this.client.setex(key, duration, value);
-  del = async (key) => await this.client.del(key);
-}
+  isAlive() { return this.isConnected; }
+
+  async get(key) { return this.getAsync(key); }
+
+  async set(key, value, duration) { return this.client.setex(key, duration, value); }
+
+  async del(key) {
+    const result = await this.client.del(key);
+    return result;
+  }
+};
 
 const redisClient = new RedisClient();
 export default redisClient;
