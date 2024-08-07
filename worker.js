eb581a1200385df13d7db4d1b23c68f33375a0cd
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-08-08 00:29:06 +0100

    eslint for worker

--- worker.js
+++ worker.js
@@ -1,41 +1,41 @@
 #!/usr/bin/node
 // Processes the files queue for generating thumbnails
 import Queue from 'bull';
-import dbClient from './utils/db';
-import {ObjectId} from 'mongodb';
+import { ObjectId } from 'mongodb';
 import imageThumbnail from 'image-thumbnail';
-import {promises as fsPromises} from 'fs';
+import { promises as fsPromises } from 'fs';
+import dbClient from './utils/db';
 
 const fileQueue = new Queue('image thumbnails');
 const userQueue = new Queue('users manager');
 
 fileQueue.process(async (job, done) => {
-  const {fileId, userId} = job.data;
+  const { fileId, userId } = job.data;
   if (!fileId) done(new Error('Missing fileId'));
   if (!userId) done(new Error('Missing userId'));
   const file = await dbClient.db.collection('files').findOne({
-    '_id': ObjectId(fileId),
-    'userId': ObjectId(userId)
+    _id: ObjectId(fileId),
+    userId: ObjectId(userId),
   });
   if (file === null) done(new Error('File not found'));
   const widths = [500, 250, 100];
-  for (const width of widths) {
+  widths.forEach(async (width) => {
     try {
-      const thumbnail = await imageThumbnail(file.localPath, {'width': width});
+      const thumbnail = await imageThumbnail(file.localPath, { width });
       await fsPromises.writeFile(`${file.localPath}_${width}`, thumbnail);
-    } catch(err) {
+    } catch (err) {
       console.log(err.message);
     }
-  }
+  });
   done();
 });
 
 userQueue.process(async (job, done) => {
-  const {userId} = job.data;
+  const { userId } = job.data;
   if (!userId) done(new Error('Missing userId'));
-  const file = await dbClient.db.collection('files').findOne({'userId': ObjectId(userId)});
+  const file = await dbClient.db.collection('files').findOne({ userId: ObjectId(userId) });
   if (file === null) done(new Error('User not found'));
-  const user = await dbClient.db.collection('users').findOne({'_id': ObjectId(userId)});
+  const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
   console.log(`Welcome ${user.email}!`);
   done();
 });
