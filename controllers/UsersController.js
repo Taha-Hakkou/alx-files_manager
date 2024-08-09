9af65e809feb5cc0d59f05c69e203a17fd727f96
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-08-09 15:59:09 +0100

    UsersController shortened

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -11,39 +11,31 @@ const userQueue = Queue('users manager');
 const UsersController = class {
   static async postNew(req, res) {
     const { email, password } = req.body;
-    if (!email) {
-      res.status(400).json({ error: 'Missing email' });
-    } else if (!password) {
-      res.status(400).json({ error: 'Missing password' });
-    }
-    let user = await dbClient.db.collection('users').findOne({ email });
-    if (user !== null) {
-      res.status(400).json({ error: 'Already exist' });
-    } else {
-      const result = await dbClient.db.collection('users').insertOne({
-        email,
-        password: sha1(password),
-      });
-      userQueue.add({ userId: result.insertedId });
-      user = await dbClient.db.collection('users').findOne({ email });
-      res.status(201).json({
-        id: user._id,
-        email: user.email,
-      });
-    }
+    if (!email) res.status(400).json({ error: 'Missing email' });
+    if (!password) res.status(400).json({ error: 'Missing password' });
+    const user = await dbClient.db.collection('users').findOne({ email });
+    if (user !== null) res.status(400).json({ error: 'Already exist' });
+    const result = await dbClient.db.collection('users').insertOne({
+      email,
+      password: sha1(password),
+    });
+    userQueue.add({ userId: result.insertedId });
+    res.status(201).json({
+      id: result.insertedId,
+      email,
+    });
   }
 
   static async getMe(req, res) {
     const userId = await redisClient.get(`auth_${req.headers['x-token']}`);
-    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
-    if (user === null) {
-      res.status(401).json({ error: 'Unauthorized' });
-    } else {
-      res.json({
-        id: user._id,
-        email: user.email,
-      });
-    }
+    const user = await dbClient.db.collection('users').findOne({
+      _id: ObjectId(userId),
+    });
+    if (user === null) res.status(401).json({ error: 'Unauthorized' });
+    res.json({
+      id: user._id,
+      email: user.email,
+    });
   }
 };
 
