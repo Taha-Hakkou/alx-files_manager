df3c70378ab1196deabc00900adb245fdd59dba1
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-08-09 23:13:17 +0100

    urlencoded added

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
 routesLoader(app);
 
 app.listen(port, () => {
